22c4af319ca54720332021254a0e436d51fc09cb
HermesC <rybakirill01@gmail.com>
2020-06-20 23:41:51 +0300

    Updates

---
+++ precache-manifest.6bb35a3f9f60eb3c1767ef77509886d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d668c307b19909b92a178b29ab3cce3",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "45fb7e6c0695d82149ef",
+    "url": "/todo/static/css/main.66389f9b.chunk.css"
+  },
+  {
+    "revision": "14a365a3b4c4ca1d2e7e",
+    "url": "/todo/static/js/2.3317a632.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/todo/static/js/2.3317a632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45fb7e6c0695d82149ef",
+    "url": "/todo/static/js/main.648d0955.chunk.js"
+  },
+  {
+    "revision": "42e23f9118876525bea8",
+    "url": "/todo/static/js/runtime-main.0d77e128.js"
+  }
+]);
